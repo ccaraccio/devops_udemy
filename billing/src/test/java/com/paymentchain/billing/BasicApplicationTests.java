b2809ed16826234ec4ee0f1d1dc8a83d8e06d669
ccaraccio <carloscaraccio@gmail.com>
2021-05-31 00:24:35 -0300

    se agrego un comentario

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 //@SpringBootTest
+// Nuevo comentario agregado por mi
 public class BasicApplicationTests {
 
 	@Test
